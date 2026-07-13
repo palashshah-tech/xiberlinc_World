8c4f9183876d4d1438e277ce7080f96e4dba131e
Palash shah <113499117+shahpalash10@users.noreply.github.com>
2026-07-14 03:38:39 +0530

    feat(voice): integrate OpenRelay TURN servers to support cross-network voice streaming

--- src/views/RoomView.js
+++ src/views/RoomView.js
@@ -39,7 +39,16 @@ const rtcConfig = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
     { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' }
+    { urls: 'stun:stun2.l.google.com:19302' },
+    {
+      urls: [
+        'turn:openrelay.metered.ca:80',
+        'turn:openrelay.metered.ca:443',
+        'turn:openrelay.metered.ca:443?transport=tcp'
+      ],
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    }
   ]
 };
 
